cc7fa7d8595fa12a9fcd58be19a0579e35f92a52
Race Carpenter <racecarpenter@gmail.com>
2017-06-02 16:13:58 -0700

    super finished

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
-//shuffle is .sort(function(){return 0.5 - Math.random()});
+//getting the object data through API call
 $.get("https://lit-fortress-6467.herokuapp.com/object").done(function(data) {
   var music = data.results;
-
-  music.sort(function() {
+  //to shuffle the object .sort(function(){return 0.5 - Math.random()});
+  var sorter = music.sort(function() {
     return 0.5 - Math.random();
   });
-
+  //creating an array of album cover images and an array of artist and album names from the shuffled object
   var covers = [];
   var artistAlbum = [];
   for (var i = 0; i < music.length; i++) {
     covers.push("images/" + music[i].cover_art);
     artistAlbum.push(music[i].title + " by " + music[i].artist);
   }
-
-
-
-
-
   console.log(artistAlbum);
-
+  //on the splash page, adding shuffled images over railroad track
   $('.first_album').append("<img src=" + covers[0] + ">");
   $('.second_album').append("<img src=" + covers[1] + ">");
   $('.third_album').append("<img src=" + covers[3] + ">");
-
-
+  //allows the select track button to take you to the playlist page
   $('.track_button').click(function() {
     window.location.href = 'playlist.html';
   });
-
+  //add album covers to the playlist page
   $('.album_1').append("<img src=" + covers[0] + ">");
   $('.album_2').append("<img src=" + covers[1] + ">");
   $('.album_3').append("<img src=" + covers[2] + ">");
   $('.album_4').append("<img src=" + covers[3] + ">");
   $('.album_5').append("<img src=" + covers[4] + ">");
-
+  //all of these click functions allow the album cover to be clicked and add the album and artist info to the text area.
   $('.album_1').click(function() {
     $('.track_list').append(artistAlbum[0] + "<br>");
   });
@@ -49,14 +43,16 @@ $.get("https://lit-fortress-6467.herokuapp.com/object").done(function(data) {
   $('.album_5').click(function() {
     $('.track_list').append(artistAlbum[4] + "<br>");
   });
-
+  //this button clears the tracks from the track list
   $('.clear_tracks').click(function() {
     $('.track_list').empty();
   })
+  //this button submits the info and then emptys the track list
   $('.submit_button').click(function() {
     var tracksubmission = $('.track_list').val();
     $.post("https://lit-fortress-6467.herokuapp.com/post", tracksubmission, function(data) {
-      console.log(tracksubmission);
+      alert("you did it. you actually did it.");
+      $('.track_list').empty();
     })
   });
 })
